54281ed3a67a890e6837ae0b8af4ee2134f8879e
abdiflash <abdiflash@gmail.com>
2026-01-24 12:11:09 +0700

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    // Identitas Google Sheet Klien
+    SHEET_ID: '1ncjWQ9ZUAZXTppHcKKH5WrivrfINIrHnAO1BQOgp7EY',
+    SHEET_GID: '0',
+
+    // Identitas Sekolah (Untuk Footer & Branding)
+    SCHOOL_NAME: 'Sekolah Edueasy',
+    SCHOOL_SITE: 'www.edueasy.id',
+    SCHOOL_URL: 'https://www.edueasy.id'
+};
