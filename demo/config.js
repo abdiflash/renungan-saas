8be77ac590602ea648b2df7384ef5182b27caef6
abdiflash <abdiflash@gmail.com>
2026-01-27 12:00:23 +0700

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -1,8 +1,10 @@
 const CONFIG = {
     SCHOOL_NAME: "Demo Renungan EduEasy",
-    SCHOOL_SITE: "edueasy.id", // Tambahkan ini
-    SCHOOL_URL: "https://edueasy.id", // Tambahkan ini
-    SHEET_ID: "1ncjWQ9ZUAZXTppHcKKH5WrivrfINIrHnAO1BQOgp7EY",
+    SCHOOL_SITE: "edueasy.id",
+    SCHOOL_URL: "https://edueasy.id",
+    SHEET_ID: "1ncjWQ9ZUAZXTppHcKKH5WrivrfINIrHnAO1BQOgp7EY", // Pastikan ID ini benar
+    SPREADSHEET_ID: "1ncjWQ9ZUAZXTppHcKKH5WrivrfINIrHnAO1BQOgp7EY", // Tambahkan ini sebagai cadangan
+    SHEET_NAME: "renungan", // Sesuaikan dengan nama tab di Google Sheet Anda
     SHEET_GID: "0",
     COUNTER_NAMESPACE: "demo-edueasy",
     COUNTER_KEY: "visitor_count"
