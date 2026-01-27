8a2c2f8952ef147f87e9d961cfff8679a11e8ec8
abdiflash <abdiflash@gmail.com>
2026-01-27 15:35:00 +0700

    Update config.js

--- athalia/config.js
+++ athalia/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     SCHOOL_NAME: "Demo Sekolah Athalia",
     SCHOOL_SITE: "sekolahathalia.sch.id",
     SCHOOL_URL: "https://sekolahathalia.sch.id",
-    SHEET_ID: "1ncjWQ9ZUAZXTppHcKKH5WrivrfINIrHnAO1BQOgp7EY",
+    SHEET_ID: "1UvENRv7Xf7OKQBKqNnRtWcRfYXJom-Rcke4hONT4SxY",
     SHEET_NAME: "renungan", // Tambahkan ini sesuai nama tab di Google Sheet
     SHEET_GID: "0",
     COUNTER_NAMESPACE: "athalia-renungan",
