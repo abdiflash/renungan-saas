9dbff85b4d3cef5d8bb8395f47e5f597e41b1ada
abdiflash <abdiflash@gmail.com>
2026-01-27 10:18:28 +0700

    Create config.js

---
+++ demo/config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    SCHOOL_NAME: "Demo Renungan EduEasy",
+    SHEET_ID: "ID_SHEET_MASTER_ANDA", // Ganti dengan ID Sheet Demo Anda
+    SHEET_GID: "0",
+    COUNTER_NAMESPACE: "demo-edueasy",
+    COUNTER_KEY: "visitor_count"
+};
