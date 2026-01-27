7af05d2f0c88e2c6e0846b15415588d6f68e08ad
abdiflash <abdiflash@gmail.com>
2026-01-27 10:20:54 +0700

    Update config.js

--- demo/config.js
+++ demo/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     SCHOOL_NAME: "Demo Renungan EduEasy",
-    SHEET_ID: "ID_SHEET_MASTER_ANDA", // Ganti dengan ID Sheet Demo Anda
+    SHEET_ID: "1ncjWQ9ZUAZXTppHcKKH5WrivrfINIrHnAO1BQOgp7EY", // Ganti dengan ID Sheet Demo Anda
     SHEET_GID: "0",
     COUNTER_NAMESPACE: "demo-edueasy",
     COUNTER_KEY: "visitor_count"
