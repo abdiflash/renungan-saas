315ad02dd7490e50c5298fef3b86db3089b104ea
abdiflash <abdiflash@gmail.com>
2026-01-27 11:34:12 +0700

    Update config.js

--- athalia/config.js
+++ athalia/config.js
@@ -1,6 +1,8 @@
 const CONFIG = {
     SCHOOL_NAME: "Sekolah Athalia",
-    SHEET_ID: "ID_SPREADSHEET_ATHALIA_ANDA", // Ganti dengan ID asli
+    SCHOOL_SITE: "sekolahathalia.sch.id",
+    SCHOOL_URL: "https://sekolahathalia.sch.id",
+    SHEET_ID: "ID_SHEET_ATHALIA_DISINI", 
     SHEET_GID: "0",
     COUNTER_NAMESPACE: "athalia-renungan",
     COUNTER_KEY: "visitor_count"
